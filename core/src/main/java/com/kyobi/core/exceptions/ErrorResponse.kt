3676f1ea6f2cc5fab59ce0e50069226a8bc4c8e8
Duy <duypq2610@gmail.com>
2025-04-28 20:11:22 +0700

    fix

--- core/src/main/java/com/kyobi/core/exceptions/ErrorResponse.kt
+++ core/src/main/java/com/kyobi/core/exceptions/ErrorResponse.kt
@@ -3,7 +3,7 @@ package com.kyobi.core.exceptions
 import com.squareup.moshi.Json
 
 data class ErrorResponse(
-    @Json(name = "message") val message: String?,
-    @Json(name = "error") val error: String?,
-    @Json(name = "statusCode") val statusCode: Int?
+    @Json(name = "message") val message: String?  = null,
+    @Json(name = "error") val error: String? = null,
+    @Json(name = "statusCode") val statusCode: Int? = null
 )
