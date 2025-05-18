f723fb1b09f447588cdc1a4f4abb6411acd52136
Duy <duypq2610@gmail.com>
2025-05-18 22:25:30 +0700

    update dataa mock

--- feature/trend/src/main/java/com/kyobi/trend/model/Reel.kt
+++ feature/trend/src/main/java/com/kyobi/trend/model/Reel.kt
@@ -2,14 +2,22 @@ package com.kyobi.trend.model
 
 data class Reel(
     val id: String,
+    val userId: String,
     val videoUrl: String,
+    val text: String,
+    val musicUrl: String? = null,
+    val parentReelId: String? = null,
     val shortenUrl: String,
-    val thumbnailUrl: String?,
+    val thumbnailUrl: String,
+    val thumbnailGif: String,
     val status: String,
     val likeCount: Int,
     val commentCount: Int,
     val shareCount: Int,
     val viewCount: Int,
     val createdAt: String,
+    val updatedAt: String,
+    val originalDuration: Double,
+    val shortenDuration: Double,
     val tags: List<String> = emptyList()
 )
